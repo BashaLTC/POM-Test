aaf52c1ce1e69a120fce54d9a00a25c7da9c39ef
BashaLTC <43848925+BashaLTC@users.noreply.github.com>
2018-10-04 17:33:53 +0530

    Add files via upload

---
+++ JenkinsDemo/src/example/Examplethree.java
@@ -0,0 +1,39 @@
+package example;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Examplethree {
+
+	@Test
+	public void demo() throws InterruptedException{
+System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+		
+		ChromeDriver driver = new ChromeDriver();
+		
+		//WebDriver driver=new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.irctc.co.in");
+		
+		driver.findElementByXPath("//span[contains(text(),'AGENT LOGIN')]").click();
+			
+		Thread.sleep(3000);
+		
+		driver.findElementById("usernameId").sendKeys("PremKumar");
+	
+		driver.findElementById("usernameId").clear();
+		
+		driver.findElementByLinkText("Sign up").click();
+		
+		Thread.sleep(5000);
+		
+		driver.close();
+	}
+	
+	
+	
+	
+	
+}
